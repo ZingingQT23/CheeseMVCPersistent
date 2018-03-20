79ec9d881e3a66ce69d4c5c9395a272c99301ea0
Dylan Kenyon <34289454+ZingingQT23@users.noreply.github.com>
2018-03-20 13:50:01 -0400

    Created Cheese Category model

---
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CheeseMVC.Models
+{
+    public class CheeseCategory
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
