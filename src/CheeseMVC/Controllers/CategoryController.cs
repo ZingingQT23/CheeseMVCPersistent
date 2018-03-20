06dade93a156342b5d246a52b90cab1467a0c406
Dylan Kenyon <34289454+ZingingQT23@users.noreply.github.com>
2018-03-20 13:49:25 -0400

    Created the Cheese Category controller

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using CheeseMVC.ViewModels;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List <CheeseCategory> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+            return View(addCheeseViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory;
+                newCategory.Name = addCheeseViewModel.Name;
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("/Category");
+            }
+
+            return View(addCheeseViewModel);
+        }
+
+    }
+}
